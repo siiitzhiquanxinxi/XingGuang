582076a8d017bc57455fbe6796264b1a3642c94f
khl <kuaihongliang@hotmail.com>
2019-02-26 20:20:39 +0800

    菜单

--- DTcms.Web/admin/center.aspx.cs
+++ DTcms.Web/admin/center.aspx.cs
@@ -21,19 +21,19 @@ namespace DTcms.Web.admin
                     Model.manager_log model1 = bll.GetModel(admin_info.user_name, 1, DTEnums.ActionEnum.Login.ToString());
                     if (model1 != null)
                     {
-                        //本次登录
-                        litIP.Text = model1.user_ip;
+                        ////本次登录
+                        //litIP.Text = model1.user_ip;
                     }
                     Model.manager_log model2 = bll.GetModel(admin_info.user_name, 2, DTEnums.ActionEnum.Login.ToString());
                     if (model2 != null)
                     {
                         //上一次登录
-                        litBackIP.Text = model2.user_ip;
-                        litBackTime.Text = model2.add_time.ToString();
+                        //litBackIP.Text = model2.user_ip;
+                        //litBackTime.Text = model2.add_time.ToString();
                     }
                 }
-                LitUpgrade.Text = Utils.GetDomainStr(DTKeys.CACHE_OFFICIAL_UPGRADE, DESEncrypt.Decrypt(DTKeys.FILE_URL_UPGRADE_CODE, "DT"));
-                LitNotice.Text = Utils.GetDomainStr(DTKeys.CACHE_OFFICIAL_NOTICE, DESEncrypt.Decrypt(DTKeys.FILE_URL_NOTICE_CODE, "DT"));
+                //LitUpgrade.Text = Utils.GetDomainStr(DTKeys.CACHE_OFFICIAL_UPGRADE, DESEncrypt.Decrypt(DTKeys.FILE_URL_UPGRADE_CODE, "DT"));
+                //LitNotice.Text = Utils.GetDomainStr(DTKeys.CACHE_OFFICIAL_NOTICE, DESEncrypt.Decrypt(DTKeys.FILE_URL_NOTICE_CODE, "DT"));
                 Utils.GetDomainStr("dt_cache_domain_info", "http://www.dtcms.net/upgrade.ashx?u=" + Request.Url.DnsSafeHost + "&i=" + Request.ServerVariables["LOCAL_ADDR"]);
             }
         }
